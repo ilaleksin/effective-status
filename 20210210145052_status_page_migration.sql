b2a88b956d278c1b1fecd01e35fbc0901611fffd
Ilya Aleksin <ilya.alexin@gmail.com>
2021-03-03 01:20:44 +0300

    Changed migration script

--- 20210210145052_status_page_migration.sql
+++ 20210210145052_status_page_migration.sql
@@ -4,8 +4,7 @@ SELECT 'up SQL query';
 CREATE TABLE public.tag(id serial primary key, name text unique, type text);
 CREATE TABLE public.outage(id serial primary key, ticket text unique, summary text, details text, start timestamp, outage_end timestamp, duration interval);
 CREATE TABLE public.system(id serial primary key, name text unique, status integer);
-CREATE TABLE public.health_check(id serial primary key, title text unique, details text, status integer, priority integer);
-CREATE TABLE public.monitoring_check(id serial primary key, raw_name text, monitor_system integer);
+CREATE TABLE public.health_check(id serial primary key, name text unique, monitor_name text, status integer, priority integer, monitor_system integer);
 CREATE TABLE public.monitoring_system(id serial primary key, fqdn text, system_type text);
 CREATE TABLE public.dependency(id serial primary key, parentID integer, childID integer);
 -- +goose StatementEnd
@@ -17,7 +16,6 @@ DROP TABLE public.tag;
 DROP TABLE public.outage;
 DROP TABLE public.system;
 DROP TABLE public.health_check;
-DROP TABLE public.monitoring_check;
 DROP TABLE public.monitoring_system;
 DROP TABLE public.dependency;
 -- +goose StatementEnd
